03884598c6774cc95c2234249dc1797c7e45074c
overtonr <rebeccaoverton@Rebeccas-MacBook-Air.local>
2023-04-02 21:53:29 -0700

    api for city to get cord

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var userForm = document.querySelector("#search-form");
+//select from states
+//https://api.openweathermap.org/geo/1.0/direct?q=portland,OR,us&limit=5&appid=891d5adf6f627c8e1d4185e6ee80e104
 
 function formSubmit(event) {
   event.preventDefault();
 
   var searchVal = document.querySelector("#search-input").value;
+//   var stateInpV = document.querySelector('#state-input').value;
   console.log(searchVal);
 
   if (!searchVal) {
@@ -12,6 +15,7 @@ function formSubmit(event) {
   }
 
   //   var queryString = './search-res.html?q=' + searchVal ;
+//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
 
   //   location.assign(queryString);
   else console.log("search for " + searchVal);
--- search-res.js
+++ search-res.js
@@ -1,37 +1,36 @@
 var userForm = document.querySelector("#user-form");
-var cityInput = document.querySelector("#city-input");
+// var cityInput = document.querySelector("#city-input");
 var currentContainer = document.querySelector("#current-container");
-var forecastContainer =  document.querySelector("#forecast-container");
+// var forecastContainer =  document.querySelector("#forecast-container");
 
 
 
-var formSubmit = function(event){
-    // preventDefault();
-    //trim excess spaces (leading and trailing),if there is still is a space, it is between words in the city, replace with "+"
-    var city = cityInput.value.trim();
-    var citySearch = city.replace(" ","+")
-    if (citySearch) {
-        getCurrent(citySearch);
-        cityInput.value ="";
-    }
-    return citySearch;
-};
+// var formSubmit = function(event){
+//     // preventDefault();
+//     //trim excess spaces (leading and trailing),if there is still is a space, it is between words in the city, replace with "+"
+//     var citySearch = city.replace(" ","+")
+//     if (citySearch) {
+//         getCurrent(citySearch);
+//         cityInput.value ="";
+//     }
+//     return citySearch;
+// };
 
-var getCurrent = function(citySearch){
-    var currentAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=891d5adf6f627c8e1d4185e6ee80e104";
-    // console.log(currentAPI);
-    fetch(currentAPI)
-    .then(function(response){ 
-    return response.json();
-    console.log(response.json())
-})
-    .then(function(response) {
-    for (var i = 0; i < response.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = response[i];
-        currentContainer.appendChild(listItem);
-};
-    })};
+// var getCurrent = function(citySearch){
+//     var currentAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=891d5adf6f627c8e1d4185e6ee80e104";
+//     // console.log(currentAPI);
+//     fetch(currentAPI)
+//     .then(function(response){ 
+//     return response.json();
+//     console.log(response.json())
+// })
+//     .then(function(response) {
+//     for (var i = 0; i < response.length; i++) {
+//         var listItem = document.createElement('li');
+//         listItem.textContent = response[i];
+//         currentContainer.appendChild(listItem);
+// };
+//     })};
 // getCurrent();
 
 
@@ -94,5 +93,63 @@ var getCurrent = function(citySearch){
 
 // need to define var savedCityButton : ID of stored city once it is appended
     //class="btn-outline-secondary rounded ml-1 m-2"
-userForm.addEventListener("submit",formSubmit);
+// userForm.addEventListener("submit",formSubmit);
 // savedCityButton.addEventListener("click", buttonClick);
+
+
+
+//user input city -- API call for coordinates
+
+//http://api.openweathermap.org/geo/1.0/direct?q=san+francisco&limit=5&appid=891d5adf6f627c8e1d4185e6ee80e104
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // getCord();
+    var cityName = document.querySelector('#city-input').value.trim();
+
+    console.log(cityName);
+
+    // var cityname = nameInputEl.value.trim();
+    // var cityInput = document.querySelector("#city-input");
+  
+    if (cityName) {
+      getCord(cityName);
+  
+      
+      userForm.value = '';
+    } else {
+      console.log('Please enter a GitHub cityName');
+    }
+  };
+
+  
+// var city = "san+francisco"
+function getCord(city) {
+    var corQueryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=891d5adf6f627c8e1d4185e6ee80e104';
+  
+  
+console.log(corQueryUrl);
+fetch(corQueryUrl)
+.then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      console.log(data, city);
+    //   response.json();
+    });
+  } else {
+    console.log('Error: ' + response.statusText);
+  }
+})
+
+.catch(function (error) {
+  console.log('Unable to connect to GitHub');
+});
+};
+
+
+  userForm.addEventListener('submit',formSubmitHandler);
+
+//after API for coordinates made, API for forecast using the values from the prev call
+// function that renders data for searched city onto screem
+// buttons appended allow for users to search for prev searches
